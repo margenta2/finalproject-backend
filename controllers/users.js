58d6517c0890f2084f1bae1b79d70ce022420bbd
Margie Ball <margie_ball@glic.com>
2021-07-01 18:05:19 -0400

    added controller functions

---
+++ controllers/users.js
@@ -0,0 +1,66 @@
+const User = require('../models').User;
+const Post = require('../models').Movie;
+
+const signup = (req, res) => {
+    User.create(req.body)
+    .then(newUser => {
+        res.json(newUser)
+    })
+}
+
+const login = (req, res) => {
+    User.findOne({
+        where: {
+            username: req.body.username,
+            password: req.body.password
+        }
+    })
+    .then(foundUser => {
+        res.json(foundUser)
+
+    })
+}
+
+const getProfile = (req, res) => {
+    User.findByPk(req.params.index)
+    .then(userProfile => {
+        Post.findAll()
+        .then(allPosts => {
+            res.json ({
+                user: userProfile,
+                post: allPosts
+            })
+        })
+    })
+}
+
+const editProfile = (req, res) => {
+    User.update(req.body, {
+        where: {
+            id: req.params.index
+        },
+        returning: true
+    })
+    .then(updatedUser => {
+        res.json(updatedUser)
+    })
+}
+
+const deleteUser = (req, res) => {
+    User.destroy({
+        where: {
+            id: req.params.index
+        }
+    })
+    .then(deletedUser => {
+        res.json(deletedUser)
+    })
+}
+
+module.exports = {
+    signup,
+    login,
+    getProfile,
+    editProfile,
+    deleteUser
+}
