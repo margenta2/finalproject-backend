9ac2ff462fa36ea29b68cb16198804a9d65dec29
Margie Ball <margie_ball@glic.com>
2021-07-06 15:49:07 -0400

    updated posts controllers routes

--- controllers/posts.js
+++ controllers/posts.js
@@ -7,6 +7,13 @@ const getAllPosts = (req, res) => {
     })
 }
 
+const createPost = (req, res) => {
+    Post.create(req.body)
+    .then(newPost => {
+        res.json(newPost)
+    })
+}
+
 const getPostById = (req, res) => {
     Post.findByPk(req.params.postId)
     .then(foundPost => {
@@ -14,7 +21,30 @@ const getPostById = (req, res) => {
     })
 }
 
+const editPost = (req, res) => {
+    Post.update(req.body, {
+        where: {
+            id: req.params.index
+        },
+        returning: true
+    })
+    .then(updatedPost => {
+        res.json(updatedPost)
+    })
+}
+
+const deletePost = (req, res) => {
+    Post.destory({
+        where: {
+            id: req.params.index
+        }
+    })
+}
+
 module.exports = {
     getAllPosts,
-    getPostById
+    getPostById,
+    createPost,
+    editPost,
+    deletePost
 }
--- routes/posts.js
+++ routes/posts.js
@@ -5,4 +5,10 @@ const ctrl = require('../controllers');
 router.get('/posts', ctrl.posts.getAllPosts);
 router.get('/postId', ctrl.posts.getPostById);
 
+router.post('/create', ctrl.posts.createPost);
+
+router.put('/posts/:index', ctrl.posts.editPost);
+
+router.delete('/posts/:index', ctrl.posts.deletePost);
+
 module.exports = router;
